66da6461deaea2946e9be89db3c1dc33bbbbe8b5
Felixpc <Felix@vonLudowig.de>
2019-10-11 14:13:20 +0200

    geht wieder

--- Test2/core/src/com/test2/game/GameScreen.java
+++ Test2/core/src/com/test2/game/GameScreen.java
@@ -8,7 +8,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.*;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import jdk.javadoc.internal.tool.Start;
+//import jdk.javadoc.internal.tool.Start;
 
 
 import java.util.Timer;
