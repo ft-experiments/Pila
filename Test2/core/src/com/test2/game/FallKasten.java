2f15ce4e79aa29166a8469f8d3e8667a61b263ea
Felixpc <Felix@vonLudowig.de>
2019-10-12 09:38:57 +0200

    Revert "exist fallbox müste funzen"
    This reverts commit ff2b918965b2bf474b29d62f9e10f83b475439b4.

--- Test2/core/src/com/test2/game/FallKasten.java
+++ Test2/core/src/com/test2/game/FallKasten.java
@@ -51,7 +51,6 @@ public class FallKasten {
         }
         if (y < Var.r_y - 400) {
             art = 0;
-            Var.existfallbox -= 1;
 
 
         }
@@ -70,15 +69,10 @@ public class FallKasten {
     static void trigger(int Kastenx, int Kasteny) {
         FallKasten FK;
         System.out.println("möglich");
-        if (Math.floor(Math.random() * Math.floor(3)) == 1 && Var.existfallbox <= 3) {
+        if (Math.floor(Math.random() * Math.floor(3)) == 1) {
             System.out.println("placed");
-            int zblock = (int) Math.floor((Math.random() * 8));
-            FK = new FallKasten(Kastenx, Kasteny, zblock);
+            FK = new FallKasten(Kastenx, Kasteny, (int) Math.floor((Math.random() * 8)));
             Fallkasten.add(FK);
-            if(zblock <= 0 && zblock <= 6){
-                Var.existfallbox=Var.existfallbox+1;
-            }
-
 
         }
 
