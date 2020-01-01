2e30302fddb073b7edecff9968e07f629dc6c7a5
Felixpc <Felix@vonLudowig.de>
2020-01-01 11:52:10 +0100

    so

--- Test2/core/src/com/test2/game/OptionScreen.java
+++ Test2/core/src/com/test2/game/OptionScreen.java
@@ -734,9 +734,9 @@ if(Gdx.input.isTouched() || c==0){
 
 if(tempold!=temp){
     System.out.println("änderung");
-    if(Var.sound_effects) {
+    //if(Var.sound_effects==true) {
         sound.play(1);
-    }
+    //}
     if(Var.beiballberurungvibrieren==1) {
         Gdx.input.vibrate(20);
     }
