7a474d8e5c7a13da88689665e65e522d4ce75459
Felixpc <Felix@vonLudowig.de>
2020-01-01 15:49:54 +0100

    a

--- Test2/core/src/com/test2/game/Kaesten.java
+++ Test2/core/src/com/test2/game/Kaesten.java
@@ -20,6 +20,7 @@ int mx2=0;
 int my2=0;
 
 
+
     Kaesten(int _kx, int _ky, int _kw, int _kh,int _art) {
         K_x = _kx;
         K_y = _ky;
@@ -39,42 +40,29 @@ int my2=0;
            // System.out.println(Gdx.graphics.getHeight() - Var.KY);           Ball.y > Gdx.graphics.getHeight() - (Gdx.graphics.getHeight() - K_y) - Ball.r/4  && Ball.x > K_x && Ball.x < K_x+100 && Ball.y < Gdx.graphics.getHeight() - (Gdx.graphics.getHeight() - K_y) - Ball.r/4+50
             if (Ball.y > K_y-Ball.r*2 && Ball.x >  K_x-Ball.r*2 && Ball.x < K_x+K_w && Ball.y < K_y+K_h+Ball.r*2) {                           //Ball.y > K_y-Ball.r*2 && Ball.x >  K_x-Ball.r*2 && Ball.x < K_x+K_w && Ball.y < K_y+K_h
 
-                mx=K_x+3;
-                my=K_y+2;
-                mx2=K_x+K_w-3;
-                my2=K_y+K_h-2;
+
 
 
                 if(Var.ballmode==0) {
-                    if (K_art != 5){
 
+                    if(Ball.bx<0){
+                        System.out.println(Ball.bx);
+                        Ball.richtungsumkehr_x();
+                        Ball.x=Ball.x-20;
+                    }else
+                    if(Ball.bx>0){
+                        System.out.println(Ball.bx);
+                        Ball.richtungsumkehr_x();
+                        Ball.x=Ball.x+20;
+                    }
 
-                        if (Ball.y < my) {
-                            Ball.richtungsumkehr_y();
-                            Ball.y -= Ball.by * 2;
-                        }
-                    if (Ball.y > my2) {
+                    if(Ball.by<0){
                         Ball.richtungsumkehr_y();
-                        Ball.y -= Ball.by * 2;
                     }
-                    if (Ball.x < mx) {
-                        Ball.richtungsumkehr_x();
-                        Ball.x -= Ball.bx * 2;
-                    }
-                    if (Ball.x > mx2) {
-                        Ball.richtungsumkehr_x();
-                        Ball.x -= Ball.bx * 2;
+                    if(Ball.by>0){
+                        Ball.richtungsumkehr_y();
                     }
 
-
-                    int random=(int)(Math.random()*2);
-                    Ball.bx=Ball.bx+random/2;
-                    Ball.by=Ball.by-random/2;
-
-
-
-
-                }
 
                    // Ball.bx = Ball.bx * -1;
                 }
