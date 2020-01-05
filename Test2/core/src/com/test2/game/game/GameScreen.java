7adba194b0c7a4b69e27c04643f328d5c082e003
Felixpc <Felix@vonLudowig.de>
2020-01-05 11:53:48 +0100

    Bälle statt Leben

--- Test2/core/src/com/test2/game/game/GameScreen.java
+++ Test2/core/src/com/test2/game/game/GameScreen.java
@@ -510,7 +510,7 @@ public static double gamestcreendelta=0;
         batch.draw(status_bar,0,Gdx.graphics.getHeight()-Gdx.graphics.getHeight()/25,Gdx.graphics.getWidth(),Gdx.graphics.getHeight()/25);
         batch.setColor(1,1,1,1f);
         font.getData().setScale(Gdx.graphics.getWidth()/720f,Gdx.graphics.getHeight()/1184f);
-        font.draw(batch, "Leben:" + Var.leben, Gdx.graphics.getWidth()-Gdx.graphics.getWidth()/3.85714f, Gdx.graphics.getHeight()-5);
+        font.draw(batch, "Bälle:" + Var.leben, Gdx.graphics.getWidth()-Gdx.graphics.getWidth()/3.85714f, Gdx.graphics.getHeight()-5);
         font.draw(batch, "Level:" + Level.Le + " ", Gdx.graphics.getWidth()/24*7, Gdx.graphics.getHeight()-5);
         //font.getData().setScale(3);
 
